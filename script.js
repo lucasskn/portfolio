45f58d76ab7d4b82b666d578b11a03031bbcb691
Lucas Klein <77367117+lucasskn@users.noreply.github.com>
2023-03-20 00:07:10 -0300

    fix: update animation smooth with scroll position

--- script.js
+++ script.js
@@ -6,20 +6,37 @@ window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY + window.innerHeight;
   const textPosition = about.offsetTop + about.offsetHeight;
 
-  if (scrollPosition >= textPosition && !isTextVisible && window.innerHeight) {
+  if (scrollPosition >= 1040 && !isTextVisible && window.innerHeight) {
     about.classList.add(
       "animate__animated",
       "animate__bounceInLeft",
       "animate__faster"
     );
+
     isTextVisible = true;
-    console.log("funcionando");
-  } else if (scrollPosition < textPosition && isTextVisible) {
+  } else if (scrollPosition < 1040 && isTextVisible) {
     about.classList.remove(
       "animate__animated",
       "animate__bounceInLeft",
       "animate__faster"
     );
     isTextVisible = false;
+  }
+});
+
+const mainskills = document.querySelector(".mainskills");
+
+let isSkillsVisible = false;
+
+window.addEventListener("scroll", () => {
+  const positionScroll = window.scrollY + window.innerHeight;
+  const skillPosition = mainskills.offsetTop + mainskills.offsetHeight;
+
+  if (positionScroll >= 1280 && !isSkillsVisible && window.innerHeight) {
+    mainskills.classList.add("animate__animated", "animate__fadeIn");
+    isSkillsVisible = true;
+  } else if (positionScroll < 1280 && isSkillsVisible) {
+    mainskills.classList.remove("animate__animated", "animate__fadeIn");
+    isSkillsVisible = false;
   }
 });
